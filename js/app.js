cd8426158141851a4a2ed1d01d9c69c99597b10d
FatemeJbr <150137395+ftmjbr-1997@users.noreply.github.com>
2023-11-09 20:02:55 +0330

    set feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-let inputElem=document.getElementById("exampleFormControlInput1")
+let inputElem=document.querySelector("#addTodo")
 let addBtnElem=document.querySelector(".btn")
 let filterElem=document.getElementById("filter")
 let sortElem=document.getElementById("sort")
+
+const addTodoHandler=()=>{
+    console.log(inputElem.Value)
+
+     (inputElem.Value) ? console.log("okey") : console.log("not")
+    
+}
+
+addBtnElem.addEventListener("click", addTodoHandler)
 
