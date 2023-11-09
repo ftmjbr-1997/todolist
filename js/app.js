b304f112afb24dfc4953cc7dbdc212b6f81ac698
unknown <FatemeJbr1997@gmail.com>
2023-11-09 14:11:57 +0330

    add template file

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let inputElem=document.getElementById("exampleFormControlInput1")
+let addBtnElem=document.querySelector(".btn")
+let filterElem=document.getElementById("filter")
+let sortElem=document.getElementById("sort")
+
