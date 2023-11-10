b0230580f5c38ed7c3a069dd0a5e0a9d9905474c
FatemeJbr <150137395+ftmjbr-1997@users.noreply.github.com>
2023-11-10 21:35:32 +0330

    add set/get todo in localstorage

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ let filterElem = document.querySelector("#filter")
 let allTodoes = []
 
 
+
+const getAllTodoes = () => {
+    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    if (localStorageTodos) {
+        todosArray = localStorageTodos
+    } else {
+        todosArray = []
+    }
+    createdTodoHandler(todosArray)
+}
+
+const setAllTodoes = (todosList) => {
+    localStorage.setItem('todos', JSON.stringify(todosList))
+}
+
 const addTodoTimeHandler = () => {
     let d = new Date()
     let Currentyear = d.getFullYear()
@@ -15,8 +30,16 @@ const addTodoTimeHandler = () => {
 
 
 const checkedHandler = (event) => {
-    let mainTodo = allTodoes.find((todo) => todo.id === event.target.id)
-    mainTodo.checked = !mainTodo.checked
+    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    let todosArray = [...localStorageTodos]
+    todosArray.forEach(function (todo) {
+        if (todo.id === event.target.id) {
+            todo.checked = !todo.checked
+        }
+    })
+    setAllTodoes(todosArray)
+    createdTodoHandler(localStorageTodos)
+
 }
 
 const addTodoHandler = () => {
@@ -27,69 +50,78 @@ const addTodoHandler = () => {
             checked: false,
             timeOfAddTodo: addTodoTimeHandler()
         }
-        allTodoes.push(newTodo)
-        createdTodoHandler(...allTodoes)
+        let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+        if (localStorageTodos) {
+            todosArray = localStorageTodos
+        } else {
+            todosArray = []
+        }
+        todosArray.push(newTodo)
+        setAllTodoes(todosArray)
+        getAllTodoes()
         inputElem[0].value = ""
     }
     else { prompt("add modal") }
 
 }
 
-const createdTodoHandler = (...todoes) => {
-    containerElem.innerHTML = ""
-    todoes.forEach((todo) => {
-        containerElem.insertAdjacentHTML("afterbegin",
-            `<ul class="list-group list-group-horizontal rounded-0 bg-transparent">
-         <li
-          class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">
-            <div class="form-check">
-              <input class="form-check-input me-0" type="checkbox" value="" onChange=checkedHandler(event)
-             ${(todo.checked) ? "checked" : ""} id=${todo.id} aria-label="..."  />
+const createdTodoHandler = (todosArray) => {
+        containerElem.innerHTML = ""
+        todosArray.forEach((todo) => {
+            containerElem.insertAdjacentHTML("afterbegin",
+                `<ul class="list-group list-group-horizontal rounded-0 bg-transparent">
+             <li
+              class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">
+                <div class="form-check">
+                  <input class="form-check-input me-0" type="checkbox" value="" onChange=checkedHandler(event)
+                 ${(todo.checked) ? "checked" : ""} id=${todo.id} aria-label="..."  />
+                </div>
+             </li>
+             <li
+              class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">
+              <p class="lead fw-normal mb-0">${todo.title}</p>
+             </li>
+             <li class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">
+            <div class="d-flex flex-row justify-content-end mb-1">
+                <a href="#!" class="text-info" data-mdb-toggle="tooltip"
+                    title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a>
+                <a href="#!" class="text-danger" data-mdb-toggle="tooltip"
+                    title="Delete todo"><i class="fas fa-trash-alt"></i></a>
             </div>
-         </li>
-         <li
-          class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">
-          <p class="lead fw-normal mb-0">${todo.title}</p>
-         </li>
-         <li class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">
-        <div class="d-flex flex-row justify-content-end mb-1">
-            <a href="#!" class="text-info" data-mdb-toggle="tooltip"
-                title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a>
-            <a href="#!" class="text-danger" data-mdb-toggle="tooltip"
-                title="Delete todo"><i class="fas fa-trash-alt"></i></a>
-        </div>
-        <div class="text-end text-muted">
-            <a href="#!" class="text-muted" data-mdb-toggle="tooltip"
-                title="Created date">
-                <p class="small mb-0"><i class="fas fa-info-circle me-2"></i>${todo.timeOfAddTodo}
-                </p>
-            </a>
-        </div>
-        </li>
-        </ul>`)
+            <div class="text-end text-muted">
+                <a href="#!" class="text-muted" data-mdb-toggle="tooltip"
+                    title="Created date">
+                    <p class="small mb-0"><i class="fas fa-info-circle me-2"></i>${todo.timeOfAddTodo}
+                    </p>
+                </a>
+            </div>
+            </li>
+            </ul>`)
 
-    })
-
+        })
 }
 
 const filterTodoHandler = (filter) => {
-    let newtodoes = [...allTodoes]
+    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    let newtodoes = [...localStorageTodos]
     if (filter === "All") {
-        createdTodoHandler(...allTodoes)
+        createdTodoHandler(localStorageTodos)
     } else if (filter === "Completed") {
         let copytodoes = newtodoes.filter(todo => todo.checked === true)
-        createdTodoHandler(...copytodoes)
+        createdTodoHandler(copytodoes)
     } else if (filter === "Active") {
         let copytodoes = newtodoes.filter(todo => todo.checked === false)
-        createdTodoHandler(...copytodoes)
+        createdTodoHandler(copytodoes)
     }
 }
 
+window.addEventListener('load', getAllTodoes)
 
 addBtnElem.addEventListener("click", addTodoHandler)
 inputElem[0].addEventListener("keydown", (event) => {
     event.keyCode === 13 && addTodoHandler()
 })
+
 filterElem.addEventListener("change", (event) => {
     filterTodoHandler(event.target.value)
 })
